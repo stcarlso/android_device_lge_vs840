29e07a690eb81322ddb0f7f3b33c150f6e930504
Stephen Carlson <carlson8@purdue.edu>
2013-09-29 12:04:42 -0400

    Commit some very old changes that I thought were committed:
    Increase max partition count to 32 so internal storage mounts right.
    Switch charger logo back to stock.
    Attempt to fix SMS splitting, but no go.

--- device.mk
+++ device.mk
@@ -13,6 +13,17 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/prebuilt/root/init.iproj.rc:root/init.iproj.rc \
 	$(LOCAL_PATH)/prebuilt/root/init.iproj.usb.rc:root/init.iproj.usb.rc \
 	$(LOCAL_PATH)/prebuilt/root/init.qcom.sh:root/init.qcom.sh \
+	$(LOCAL_PATH)/prebuilt/root/sbin/chargerlogo:root/sbin/chargerlogo \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_charging_01.png:root/chargerimages/battery_charging_01.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_charging_02.png:root/chargerimages/battery_charging_02.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_charging_03.png:root/chargerimages/battery_charging_03.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_charging_04.png:root/chargerimages/battery_charging_04.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_charging_05.png:root/chargerimages/battery_charging_05.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_charging_06.png:root/chargerimages/battery_charging_06.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_wait_01.png:root/chargerimages/battery_wait_01.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_wait_02.png:root/chargerimages/battery_wait_02.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/battery_charging_warning.png:root/chargerimages/battery_charging_warning.png \
+	$(LOCAL_PATH)/prebuilt/root/chargerimages/bg_wireless.png:root/chargerimages/bg_wireless.png \
 	$(LOCAL_PATH)/prebuilt/root/ueventd.iproj.rc:root/ueventd.iproj.rc
 
 # Recovery
